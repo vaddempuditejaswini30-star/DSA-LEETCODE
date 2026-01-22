2dae1efb4306ede5092c075b7b3dd6bda7b70507
vaddempuditejaswini30-star <vaddempuditejaswini30@gmail.com>
2026-01-22 15:33:42 +0530

    Time: 1 ms (99.74%) | Memory: 48 MB (27.24%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+            nums[k++]=nums[i];
+            }
+        }
+        while(k<n){
+            nums[k++]=0;
+        }
+    }
+}
